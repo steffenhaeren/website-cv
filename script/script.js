08967b988f1b41191726b7fd28986f445494fd5f
Steffen Hæren <steffenhaeren@gmail.com>
2022-06-12 15:48:45 +0200

    added typing animation

---
+++ script/script.js
@@ -0,0 +1,7 @@
+/* TYPING ANIMATION SCRIPT */
+var typed = new Typed(".typing",{
+    strings:["", "Web Designer", "Web Developer", "Graphic Designer", "Software Developer", "Sexy Motherfucker"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+});
